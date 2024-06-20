051a8fc365164d4a8ee5c8803c84260bea89c772
engrtipusultan <69894984+engrtipusultan@users.noreply.github.com>
2024-06-20 08:34:59 +0000

    xcv

--- 01_02/b/src/main.rs
+++ 01_02/b/src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use std::fs;
+
+fn main() {
+    let file_path = "test_file";
+    let file_contents: String = fs::read_to_string(file_path).expect("Unable to read the file");
+    println!("{}",file_contents);
+  }
